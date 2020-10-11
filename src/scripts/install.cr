b6713261312dc57ca4bc39efb060babe88c8edf7
Ryan Westlund <rlwestlund@gmail.com>
2020-10-10 23:22:20 -0400

    Make src/scripts/install.cr use /usr/local if available

--- src/scripts/install.cr
+++ src/scripts/install.cr
@@ -3,9 +3,11 @@
 require "../cfg"
 require "ecr"
 
-File.open("/etc/nginx/nginx.conf", "w") do |file|
+prefix = File.exists?("/usr/local/etc/nginx") ? "/usr/local" : ""
+
+File.open("#{prefix}/etc/nginx/nginx.conf", "w") do |file|
   ECR.embed("cfg/nginx/nginx.conf", file)
 end
-File.open("/etc/nginx/sites/didact", "w") do |file|
+File.open("#{prefix}/etc/nginx/sites/didact", "w") do |file|
   ECR.embed("cfg/nginx/sites/didact", file)
 end
