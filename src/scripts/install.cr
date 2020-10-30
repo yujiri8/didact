e9ec6a52f9473aef2a085f82268970578bb78dc8
Ryan Westlund <rlwestlund@gmail.com>
2020-10-30 15:59:27 -0400

    Create /etc/nginx/sites if it doesn't exist

--- src/scripts/install.cr
+++ src/scripts/install.cr
@@ -8,6 +8,8 @@ prefix = File.exists?("/usr/local/etc/nginx") ? "/usr/local" : ""
 File.open("#{prefix}/etc/nginx/nginx.conf", "w") do |file|
   ECR.embed("cfg/nginx/nginx.conf", file)
 end
+# Not all distributions have this dir.
+Dir.mkdir_p("#{prefix}/etc/nginx/sites")
 File.open("#{prefix}/etc/nginx/sites/didact", "w") do |file|
   ECR.embed("cfg/nginx/sites/didact", file)
 end
