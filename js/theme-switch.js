9e0642c3ba7c09cdc9ac309f22c1ada1b6920fad
yujiri8 <rlwestlund@gmail.com>
2021-04-24 19:03:35 +0000

    Replace theme switch with native checkbox and default to dark

--- js/theme-switch.js
+++ js/theme-switch.js
@@ -1,5 +1,4 @@
 import {LitElement, html, css} from 'lit-element';
-import '@material/mwc-switch';
 
 import {styles} from './css.js';
 
@@ -11,30 +10,26 @@ customElements.define('theme-switch', class extends LitElement {
 			display: flex;
 			flex-direction: column;
 			align-items: center;
-			margin-left: 1em;
-			margin-right: 1em;
 		}
 		label {
 			margin-bottom: 0.2em;
 		}
-		mwc-switch { --mdc-theme-secondary: #0f0 }
 		`];
 	}
 	render() {
 		return html`
-		<label for="theme-switch"><small>Dark mode</small></label>
-		<mwc-switch id="theme-switch" @change="${this.toggleTheme}"
-				?checked="${document.documentElement.getAttribute('data-theme') == 'dark'}">
-		</mwc-switch>
+		<label for="theme-switch"><small>Light mode</small></label>
+		<input type="checkbox" id="theme-switch" @change="${this.toggleTheme}"
+				?checked="${document.documentElement.getAttribute('data-theme') == 'light'}">
 		`;
 	}
 	toggleTheme(e) {
 		if (e.target.checked) {
-			document.documentElement.setAttribute('data-theme', 'dark');
-			localStorage.setItem('theme', 'dark');
-		} else {
 			document.documentElement.setAttribute('data-theme', 'light');
 			localStorage.setItem('theme', 'light');
+		} else {
+			document.documentElement.setAttribute('data-theme', 'dark');
+			localStorage.setItem('theme', 'dark');
 		}
 	}
 });
