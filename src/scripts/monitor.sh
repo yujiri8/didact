d8ce5358c3a061e252318fe02671e6cc6ea201ad
yujiri8 <rlwestlund@gmail.com>
2021-06-20 09:32:43 +0000

    fix src/scripts/monitor.sh

--- src/scripts/monitor.sh
+++ src/scripts/monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # This is used to monitor changes to the content dir and automatically mirror them in production.
 
-find content | entr -a ./didact-template /_
+find content | entr -na ./didact-template /_
